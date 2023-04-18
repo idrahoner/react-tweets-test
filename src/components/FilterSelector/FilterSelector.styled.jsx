f1f85103e168c2994eddf7d01463031696f1119d
Ivan Drahoner <111982515+idrahoner@users.noreply.github.com>
2023-04-18 22:02:41 +0300

    Add style for filter selector

---
+++ src/components/FilterSelector/FilterSelector.styled.jsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const FilterContainer = styled.div`
+  width: 500px;
+  margin-top: ${(p) => p.theme.space.l}px;
+  margin-left: auto;
+  margin-right: auto;
+`;
