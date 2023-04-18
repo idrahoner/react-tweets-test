0ff7eb1444c9611605bff1dc066decb9a6af9cf1
Ivan Drahoner <111982515+idrahoner@users.noreply.github.com>
2023-04-18 15:47:19 +0300

    Reset default styles and create base theme for app

---
+++ src/components/theme.js
@@ -0,0 +1,29 @@
+export const theme = Object.freeze({
+  space: { none: 0, xs: 14, s: 20, sm: 26, m: 28, l: 48 },
+  fontSize: { m: 18, l: 20 },
+  colors: {
+    primaryBg:
+      "linear-gradient(114.99deg, #471CA9 -0.99%, #5736A3 54.28%, #4B2A99 78.99%);",
+    secondaryBg: "#5736A3",
+    regularButtonBg: "#EBD8FF",
+    activeButtonBg: "#5CD3A8",
+    accent: "#EBD8FF",
+    primaryText: "#373737",
+  },
+  fonts: { primary: "Montserrat" },
+  fontWeight: { regular: 500, medium: 600 },
+  letterSpacing: { regular: 1.2 },
+  sizes: { tweetWidth: "360px", tweetHeight: "480px" },
+  borders: [],
+  borderWidth: [],
+  borderStyles: [],
+  radii: { tweet: "20px", button: "10px", avatarFrame: "13px", circle: "50%" },
+  shadows: {
+    tweet: "-2.5777px 6.87386px 20.6216px rgba(0, 0, 0, 0.23)",
+    button: "0px 3.43693px 3.43693px rgba(0, 0, 0, 0.25)",
+    separator:
+      "0px 3.43693px 3.43693px rgba(0, 0, 0, 0.06), inset 0px -1.71846px 3.43693px #AE7BE3, inset 0px 3.43693px 2.5777px #FBF8FF",
+    avatarFrame:
+      "0px 4.39163px 4.39163px rgba(0, 0, 0, 0.06), inset 0px -2.19582px 4.39163px #AE7BE3, inset 0px 4.39163px 3.29372px #FBF8FF",
+  },
+});
